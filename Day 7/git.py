f8ae5272055ff28df6ef159b920f854a61896d20
Shrijan1stha <shrestha.shrijan36@gmail.com>
2023-05-03 09:04:27 +0545

    test commit

--- Day 7/git.py
+++ Day 7/git.py
@@ -1,1 +1,19 @@
 # git init
+# git add .  (git add --all)
+# git commmit -m "<commit_message>"
+# git remote add origin <ssh link>
+# git push -u origin <branch name>
+
+
+## git status  # Shows the current status of all staged and unstaged file.
+
+
+# Setting git configuration
+# 1. git config --global user.name "<your_username>"
+# 2. git config --global user.email "<your_email>"
+
+
+# Generating SSh key pair
+# ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+
+# git branch  # It shows all the branches and highlights the current branch
